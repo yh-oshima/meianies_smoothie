7402be6c950f0b0e09d7053dd766bf056379191c
yh-oshima <sunyohei9250@gmail.com>
2026-02-13 07:12:55 +0900

    Fix method call for converting Snowpark DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 #Convert the Snowpark Dataframe to a Pandas DataFrame so we can use the LOC function
-pd_df=my_dataframe.to_pandas
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
